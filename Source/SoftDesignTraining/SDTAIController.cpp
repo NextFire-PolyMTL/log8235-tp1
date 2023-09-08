54471fce7ae972b235882bed7a52298f962f0f38
NextFire <git@yuru.moe>
2023-09-08 16:57:59 -0400

    task 1

--- Source/SoftDesignTraining/SDTAIController.cpp
+++ Source/SoftDesignTraining/SDTAIController.cpp
@@ -5,9 +5,15 @@
 
 void ASDTAIController::Tick(float deltaTime)
 {
-
+    Super::Tick(deltaTime);
+    auto *pawn = GetPawn();
+    auto targetDir = FVector::LeftVector; // placeholder
+    // rotate
+    auto rot = FQuat::FindBetween(pawn->GetActorForwardVector(), targetDir);
+    pawn->AddActorWorldRotation(rot);
+    // move forward
+    m_Speed = FMath::Min(m_Speed + Acceleration * deltaTime, MaxSpeed);
+    pawn->AddMovementInput(pawn->GetActorForwardVector(), m_Speed * deltaTime);
+    // debug print
+    GEngine->AddOnScreenDebugMessage(-1, 0.0f, FColor::Yellow, FString::Printf(TEXT("[%s] Velocity: %f m/s"), *pawn->GetName(), pawn->GetVelocity().Size()));
 }
-
-
-
-
--- Source/SoftDesignTraining/SDTAIController.h
+++ Source/SoftDesignTraining/SDTAIController.h
@@ -8,7 +8,7 @@
 #include "SDTAIController.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS(ClassGroup = AI, config = Game)
 class SOFTDESIGNTRAINING_API ASDTAIController : public AAIController
@@ -16,4 +16,13 @@ class SOFTDESIGNTRAINING_API ASDTAIController : public AAIController
     GENERATED_BODY()
 public:
     virtual void Tick(float deltaTime) override;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float MaxSpeed = 500.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float Acceleration = 200.0f;
+
+private:
+    float m_Speed = 0.0f;
 };
