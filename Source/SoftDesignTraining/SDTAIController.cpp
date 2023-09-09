1778030928431d7652c53d655e77bf68e8b3388b
NextFire <git@yuru.moe>
2023-09-09 12:12:25 -0400

    [task1] set walk speed and accel on charamovecomp

--- Source/SoftDesignTraining/SDTAIController.cpp
+++ Source/SoftDesignTraining/SDTAIController.cpp
@@ -3,6 +3,15 @@
 #include "SDTAIController.h"
 #include "SoftDesignTraining.h"
 
+void ASDTAIController::BeginPlay()
+{
+    Super::BeginPlay();
+    auto *chara = GetCharacter();
+    auto *moveComp = chara->GetCharacterMovement();
+    moveComp->MaxWalkSpeed = MaxSpeed;
+    moveComp->MaxAcceleration = Acceleration;
+}
+
 void ASDTAIController::Tick(float deltaTime)
 {
     Super::Tick(deltaTime);
@@ -12,8 +21,7 @@ void ASDTAIController::Tick(float deltaTime)
     auto rot = FQuat::FindBetween(pawn->GetActorForwardVector(), targetDir);
     pawn->AddActorWorldRotation(rot);
     // move forward
-    m_Speed = FMath::Min(m_Speed + Acceleration * deltaTime, MaxSpeed);
-    pawn->AddMovementInput(pawn->GetActorForwardVector(), m_Speed * deltaTime);
+    pawn->AddMovementInput(pawn->GetActorForwardVector());
     // debug print
     GEngine->AddOnScreenDebugMessage(-1, 0.0f, FColor::Yellow, FString::Printf(TEXT("[%s] Velocity: %f m/s"), *pawn->GetName(), pawn->GetVelocity().Size()));
 }
--- Source/SoftDesignTraining/SDTAIController.h
+++ Source/SoftDesignTraining/SDTAIController.h
@@ -15,14 +15,12 @@ class SOFTDESIGNTRAINING_API ASDTAIController : public AAIController
 {
     GENERATED_BODY()
 public:
+    virtual void BeginPlay() override;
     virtual void Tick(float deltaTime) override;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite)
     float MaxSpeed = 500.0f;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    float Acceleration = 200.0f;
-
-private:
-    float m_Speed = 0.0f;
+    float Acceleration = 250.0f;
 };
