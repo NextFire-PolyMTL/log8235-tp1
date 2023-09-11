d4626e05e2cfbae7a458106cb2d7e057c04eec9e
pireb <pireb@L4818-14>
2023-09-11 18:33:12 -0400

    try rotation toward collectible

--- Source/SoftDesignTraining/SDTAIController.cpp
+++ Source/SoftDesignTraining/SDTAIController.cpp
@@ -53,16 +53,25 @@ void ASDTAIController::DetectCollectible()
 
     
     //world->LineTraceMultiByObjectType(hitResults, pawn->GetActorLocation(), pawn->GetActorLocation() + pawn->GetActorForwardVector() * 1000, objectQueryParams);
-    bool sthDetected = world->LineTraceSingleByObjectType(hitResult, pawn->GetActorLocation(), pawn->GetActorLocation() + pawn->GetActorForwardVector() * 1000, objectQueryParams);
+    bool sthDetected = world->LineTraceSingleByObjectType(hitResult, pawn->GetActorLocation(), pawn->GetActorLocation() + pawn->GetActorForwardVector() * 2000, objectQueryParams);
     GEngine->AddOnScreenDebugMessage(-1, 0.0f, FColor::Blue, FString::Printf(TEXT("detected=%d"), sthDetected));
     //Draw hits
     if (sthDetected) {
         //GEngine->AddOnScreenDebugMessage(-1, 0.0f, FColor::Blue, FString::Printf(TEXT("nb of hit: %d"), hitResults.Num()));
         GEngine->AddOnScreenDebugMessage(-1, 0.0f, FColor::Blue, FString::Printf(TEXT("You hit: %s"), *FString(hitResult.GetActor()->GetName())));
+        FVector vectPawnToTarget = hitResult.GetActor()->GetActorLocation() - pawn->GetActorLocation();
+        float rotAngle = acos(FVector::DotProduct(pawn->GetActorForwardVector(),vectPawnToTarget)/ vectPawnToTarget.Size())*180/PI;
+        FVector cross_prod = FVector::CrossProduct(vectPawnToTarget, pawn->GetActorForwardVector());
+        float a = FVector::DotProduct(cross_prod, FVector::UpVector);
+        if (a > 0) {
+            rotAngle = -rotAngle;
+        }
+        GEngine->AddOnScreenDebugMessage(-1, 0.0f, FColor::Blue, FString::Printf(TEXT("Angle to hit: %f"), rotAngle));
+        pawn->AddActorWorldRotation(FRotator(0, rotAngle, 0));
         DrawDebugLine(world, pawn->GetActorLocation(), hitResult.GetActor()->GetActorLocation(), FColor::Blue, false, -1, 0, 5);
     }
     else {
-        DrawDebugLine(world, pawn->GetActorLocation(), pawn->GetActorLocation() + pawn->GetActorForwardVector() * 1000, FColor::Red, false, -1, 0, 5);
+        DrawDebugLine(world, pawn->GetActorLocation(), pawn->GetActorLocation() + pawn->GetActorForwardVector() * 2000, FColor::Red, false, -1, 0, 5);
     }
 
 
