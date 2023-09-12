3af87f39e22270038daf96f7f797dd44e2f4a7b2
pireb <pireb@L4818-14>
2023-09-12 19:19:26 -0400

    Detection of collectibles with a square projection. Pb with collectibles that still collided with the raycast even when they are collected.

--- Source/SoftDesignTraining/SDTAIController.cpp
+++ Source/SoftDesignTraining/SDTAIController.cpp
@@ -22,9 +22,11 @@ void ASDTAIController::Tick(float deltaTime)
 void ASDTAIController::Move()
 {
     auto *pawn = GetPawn();
+    /*
     // rotate
     auto rot = FQuat::FindBetween(pawn->GetActorForwardVector(), TargetDir);
     pawn->AddActorWorldRotation(rot);
+    */
     // move forward
     pawn->AddMovementInput(pawn->GetActorForwardVector());
     // debug print
@@ -37,14 +39,14 @@ void ASDTAIController::DetectCollectible()
     UWorld* world = GetWorld();
     FHitResult hitResult;
     //TArray<struct FHitResult> hitResults;
-    FCollisionObjectQueryParams objectQueryParams;// = FCollisionObjectQueryParams::DefaultObjectQueryParam;
+    FCollisionObjectQueryParams objectQueryParams;
+    FCollisionShape visionBox= FCollisionShape().MakeBox(FVector(1, 100, 100));
     /*
     objectQueryParams.AddObjectTypesToQuery(ECC_PhysicsBody);
     objectQueryParams.AddObjectTypesToQuery(ECC_WorldStatic);
     objectQueryParams.AddObjectTypesToQuery(ECC_WorldDynamic);
-    objectQueryParams.AddObjectTypesToQuery(ECC_Destructible);
-    objectQueryParams.AddObjectTypesToQuery(ECC_EngineTraceChannel1);
     */
+    
 
     objectQueryParams.AddObjectTypesToQuery(ECC_GameTraceChannel5);
     
@@ -53,12 +55,14 @@ void ASDTAIController::DetectCollectible()
 
     
     //world->LineTraceMultiByObjectType(hitResults, pawn->GetActorLocation(), pawn->GetActorLocation() + pawn->GetActorForwardVector() * 1000, objectQueryParams);
-    bool sthDetected = world->LineTraceSingleByObjectType(hitResult, pawn->GetActorLocation(), pawn->GetActorLocation() + pawn->GetActorForwardVector() * 2000, objectQueryParams);
+    //bool sthDetected = world->LineTraceSingleByObjectType(hitResult, pawn->GetActorLocation(), pawn->GetActorLocation() + pawn->GetActorForwardVector() * 1000, objectQueryParams);
+    bool sthDetected = world->SweepSingleByObjectType(hitResult, pawn->GetActorLocation(), pawn->GetActorLocation() + pawn->GetActorForwardVector() * 1000, FQuat(0,0,0,0) ,objectQueryParams,visionBox);
+    
+
     GEngine->AddOnScreenDebugMessage(-1, 0.0f, FColor::Blue, FString::Printf(TEXT("detected=%d"), sthDetected));
-    //Draw hits
     if (sthDetected) {
-        //GEngine->AddOnScreenDebugMessage(-1, 0.0f, FColor::Blue, FString::Printf(TEXT("nb of hit: %d"), hitResults.Num()));
-        GEngine->AddOnScreenDebugMessage(-1, 0.0f, FColor::Blue, FString::Printf(TEXT("You hit: %s"), *FString(hitResult.GetActor()->GetName())));
+
+        
         FVector vectPawnToTarget = hitResult.GetActor()->GetActorLocation() - pawn->GetActorLocation();
         float rotAngle = acos(FVector::DotProduct(pawn->GetActorForwardVector(),vectPawnToTarget)/ vectPawnToTarget.Size())*180/PI;
         FVector cross_prod = FVector::CrossProduct(vectPawnToTarget, pawn->GetActorForwardVector());
@@ -66,12 +70,21 @@ void ASDTAIController::DetectCollectible()
         if (a > 0) {
             rotAngle = -rotAngle;
         }
-        GEngine->AddOnScreenDebugMessage(-1, 0.0f, FColor::Blue, FString::Printf(TEXT("Angle to hit: %f"), rotAngle));
+        
         pawn->AddActorWorldRotation(FRotator(0, rotAngle, 0));
+
+        //Debug drawing
         DrawDebugLine(world, pawn->GetActorLocation(), hitResult.GetActor()->GetActorLocation(), FColor::Blue, false, -1, 0, 5);
+        DrawDebugBox(world, hitResult.GetActor()->GetActorLocation(), visionBox.GetBox(), FColor::Blue, false, -1, 0, 5);
+        DrawDebugBox(world, pawn->GetActorLocation(), visionBox.GetBox(), FColor::Blue, false, -1, 0, 5);
+        GEngine->AddOnScreenDebugMessage(-1, 0.0f, FColor::Blue, FString::Printf(TEXT("You hit: %s"), *FString(hitResult.GetActor()->GetActorLabel())));
+        GEngine->AddOnScreenDebugMessage(-1, 0.0f, FColor::Blue, FString::Printf(TEXT("Angle to hit: %f"), rotAngle));
     }
     else {
-        DrawDebugLine(world, pawn->GetActorLocation(), pawn->GetActorLocation() + pawn->GetActorForwardVector() * 2000, FColor::Red, false, -1, 0, 5);
+
+        //Debug Drawing
+        DrawDebugLine(world, pawn->GetActorLocation(), pawn->GetActorLocation() + pawn->GetActorForwardVector() * 1000, FColor::Red, false, -1, 0, 5);
+        DrawDebugBox(world, pawn->GetActorLocation(), visionBox.GetBox(), FColor::Red, false, -1, 0, 5);
     }
 
 
