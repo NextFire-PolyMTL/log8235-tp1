5311d19ad4ca6803ae041b9338c5b7f16bdeccd8
pireb <pireb@L4818-14>
2023-09-13 12:12:15 -0400

    formatting

--- Source/SoftDesignTraining/SDTAIController.cpp
+++ Source/SoftDesignTraining/SDTAIController.cpp
@@ -35,45 +35,45 @@ void ASDTAIController::Move()
 
 void ASDTAIController::DetectCollectible()
 {
-    auto* pawn = GetPawn();
-    UWorld* world = GetWorld();
+    auto *pawn = GetPawn();
+    UWorld *world = GetWorld();
     float radius = VisionDistance;
     bool drawDebug = true;
-    //FHitResult hitResult;
+    // FHitResult hitResult;
     FCollisionObjectQueryParams objectQueryParams;
-    FCollisionShape visionBox= FCollisionShape().MakeBox(FVector(1, 100, 100));
+    FCollisionShape visionBox = FCollisionShape().MakeBox(FVector(1, 100, 100));
     FCollisionShape neighSphere = FCollisionShape().MakeSphere(radius);
     objectQueryParams.AddObjectTypesToQuery(ECC_GameTraceChannel5);
-    
-   
 
     TArray<struct FOverlapResult> outOverlaps;
-    bool sthDetected=world->OverlapMultiByObjectType(outOverlaps, pawn->GetActorLocation(), FQuat::Identity, objectQueryParams, neighSphere);
-    DrawDebugSphere(world,pawn->GetActorLocation(), radius, 24, FColor::Green);
+    bool sthDetected = world->OverlapMultiByObjectType(outOverlaps, pawn->GetActorLocation(), FQuat::Identity, objectQueryParams, neighSphere);
+    DrawDebugSphere(world, pawn->GetActorLocation(), radius, 24, FColor::Green);
     DrawDebugCone(world, pawn->GetActorLocation(), pawn->GetActorForwardVector(), VisionDistance, VisionAngle, VisionAngle, 24, FColor::Yellow);
-    
-    
-    //world->LineTraceMultiByObjectType(hitResults, pawn->GetActorLocation(), pawn->GetActorLocation() + pawn->GetActorForwardVector() * 1000, objectQueryParams);
-    //bool sthDetected = world->LineTraceSingleByObjectType(hitResult, pawn->GetActorLocation(), pawn->GetActorLocation() + pawn->GetActorForwardVector() * 1000, objectQueryParams);
-    //bool sthDetected = world->SweepSingleByObjectType(hitResult, pawn->GetActorLocation(), pawn->GetActorLocation() + pawn->GetActorForwardVector() * 1000, FQuat(0,0,0,0) ,objectQueryParams,visionBox);
-    
+
+    // world->LineTraceMultiByObjectType(hitResults, pawn->GetActorLocation(), pawn->GetActorLocation() + pawn->GetActorForwardVector() * 1000, objectQueryParams);
+    // bool sthDetected = world->LineTraceSingleByObjectType(hitResult, pawn->GetActorLocation(), pawn->GetActorLocation() + pawn->GetActorForwardVector() * 1000, objectQueryParams);
+    // bool sthDetected = world->SweepSingleByObjectType(hitResult, pawn->GetActorLocation(), pawn->GetActorLocation() + pawn->GetActorForwardVector() * 1000, FQuat(0,0,0,0) ,objectQueryParams,visionBox);
 
     GEngine->AddOnScreenDebugMessage(-1, 0.0f, FColor::Blue, FString::Printf(TEXT("detected=%d"), sthDetected));
-    if (sthDetected) {
-        for (int i = 0;i < outOverlaps.Num();i++) {
-            bool isVisible = IsInVisionCone(world,pawn,outOverlaps[i].GetActor());
-            if (isVisible) {
+    if (sthDetected)
+    {
+        for (int i = 0; i < outOverlaps.Num(); i++)
+        {
+            bool isVisible = IsInVisionCone(world, pawn, outOverlaps[i].GetActor());
+            if (isVisible)
+            {
                 FVector vectPawnToTarget = outOverlaps[i].GetActor()->GetActorLocation() - pawn->GetActorLocation();
                 float rotAngle = acos(FVector::DotProduct(pawn->GetActorForwardVector(), vectPawnToTarget) / vectPawnToTarget.Size()) * 180 / PI;
                 FVector cross_prod = FVector::CrossProduct(vectPawnToTarget, pawn->GetActorForwardVector());
                 float a = FVector::DotProduct(cross_prod, FVector::UpVector);
-                if (a > 0) {
+                if (a > 0)
+                {
                     rotAngle = -rotAngle;
                 }
 
                 pawn->AddActorWorldRotation(FRotator(0, rotAngle, 0));
 
-                //Debug drawing
+                // Debug drawing
                 DrawDebugLine(world, pawn->GetActorLocation(), outOverlaps[i].GetActor()->GetActorLocation(), FColor::Blue, false, -1, 0, 5);
                 DrawDebugBox(world, outOverlaps[i].GetActor()->GetActorLocation(), visionBox.GetBox(), FColor::Blue, false, -1, 0, 5);
                 DrawDebugBox(world, pawn->GetActorLocation(), visionBox.GetBox(), FColor::Blue, false, -1, 0, 5);
@@ -83,11 +83,12 @@ void ASDTAIController::DetectCollectible()
             }
         }
     }
-
 }
-bool ASDTAIController::IsInVisionCone(UWorld* world, AActor* pawn, AActor* targetActor) {
 
-    //We check if the target actor is too far from the pawn
+bool ASDTAIController::IsInVisionCone(UWorld *world, AActor *pawn, AActor *targetActor)
+{
+
+    // We check if the target actor is too far from the pawn
     if (FVector::Dist2D(pawn->GetActorLocation(), targetActor->GetActorLocation()) > VisionDistance)
     {
         return false;
@@ -105,9 +106,10 @@ bool ASDTAIController::IsInVisionCone(UWorld* world, AActor* pawn, AActor* targe
     objectQueryParams.AddObjectTypesToQuery(ECC_GameTraceChannel3);
     FHitResult hitResult;
 
-    //We see if there is an obstacle
+    // We see if there is an obstacle
     bool sthDetected = world->SweepSingleByObjectType(hitResult, pawn->GetActorLocation(), targetActor->GetActorLocation(), FQuat(0, 0, 0, 0), objectQueryParams, collisionCylinder);
-    if (sthDetected) {
+    if (sthDetected)
+    {
         return false;
     }
 
@@ -115,11 +117,11 @@ bool ASDTAIController::IsInVisionCone(UWorld* world, AActor* pawn, AActor* targe
     float value = FVector::DotProduct(direction.GetSafeNormal(), pawn->GetActorForwardVector().GetSafeNormal());
     auto angle = FMath::Acos(value);
 
-    //We check if the target actor is in the cone
-    if (FMath::Abs(angle) <= VisionAngle) {
+    // We check if the target actor is in the cone
+    if (FMath::Abs(angle) <= VisionAngle)
+    {
         return true;
     }
 
     return false;
-
 }
